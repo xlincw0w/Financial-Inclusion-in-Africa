134f934c778f693f93ff318e1e64fc4709a3466b
xlincw0w <khaled.khazem@yahoo.com>
2019-09-05 16:34:25 +0100

    0.75 accuracy

--- prog.py
+++ prog.py
@@ -73,57 +73,60 @@ def numerize_data(data):
   data = data.replace(['Secondary education', 'No formal education', 'Vocational/Specialised training', 'Primary education', 'Tertiary education', 'Other/Dont know/RTA'], [2, 0, 3, 1, 3, 2])
   data = data.replace(['Self employed', 'Government Dependent', 'Formally employed Private', 'Informally employed', 'Formally employed Government', 'Farming and Fishing', 'Remittance Dependent', 'Other Income', 'Dont Know/Refuse to answer', 'No Income'], [1, 2, 3, 0, 4, 0, 0, 1, 1, 0])
   return data
+
+
+if __name__ == '__main__':
                                        
-submissionf = read_csv('data/SubmissionFile.csv')
-vardef = read_csv('data/VariableDefinitions.csv')
-train_origin = read_csv('data/Train_v2.csv')
-test = read_csv('data/Test_v2.csv')
+    submissionf = read_csv('data/SubmissionFile.csv')
+    vardef = read_csv('data/VariableDefinitions.csv')
+    train_origin = read_csv('data/Train_v2.csv')
+    test = read_csv('data/Test_v2.csv')
 
-train_ye = train_origin[train_origin['bank_account'] == 'Yes']
-train_no = train_origin[train_origin['bank_account'] == 'No'].sample(3312)
-train_full = pd.DataFrame.append(train_ye, train_no)
-train_full.sample(frac=1).reset_index(drop=True)
+    train_ye = train_origin[train_origin['bank_account'] == 'Yes']
+    train_no = train_origin[train_origin['bank_account'] == 'No'].sample(3312)
+    train_full = pd.DataFrame.append(train_ye, train_no)
+    train_full.sample(frac=1).reset_index(drop=True)
 
-train_full = numerize_data(train_full)
+    train_full = numerize_data(train_full)
 
-features = train_full.drop(columns=['bank_account', 'uniqueid'])
-labels = train_full.pop('bank_account')
+    features = train_full.drop(columns=['bank_account', 'uniqueid'])
+    labels = train_full.pop('bank_account')
 
-train_feat, test_feat, train_lab, test_lab = train_test_split(features, labels, test_size=0.1)
+    train_feat, test_feat, train_lab, test_lab = train_test_split(features, labels, test_size=0.1)
 
-nn = neural_net()
-nn.train(train_feat.values, train_lab.values)
+    nn = neural_net()
+    nn.train(train_feat.values, train_lab.values)
 
-print()
-print('Evaluation : ')
-res = nn.evaluate(test_feat.values, test_lab.values)
+    print()
+    print('Evaluation : ')
+    res = nn.evaluate(test_feat.values, test_lab.values)
 
-#Uncomment to plot loss history
-#nn.plotloss()
+    #Uncomment to plot loss history
+    #nn.plotloss()
 
-print()
-print('Resultat : ', res)
+    print()
+    print('Resultat : ', res)
 
-#nn.save_model(res[1])
+    #nn.save_model(res[1])
 
-test = numerize_data(test)
-test_data = test.drop(columns=['uniqueid'])
-uniqueids = test.pop('uniqueid')
-predict = nn.predict(test_data)
-predict = predict.argmax(1)
-dfpred = pd.DataFrame({'bank_account':predict})
-test_data['bank_account'] = dfpred
-test_data = test_data.replace({'country': [0, 1, 2, 3]}, {'country': ['Kenya', 'Rwanda', 'Tanzania', 'Uganda'] })
-test_data['uniqueid'] = uniqueids + ' x ' + test_data['country']
-result = test_data[['uniqueid', 'bank_account']]
+    test = numerize_data(test)
+    test_data = test.drop(columns=['uniqueid'])
+    uniqueids = test.pop('uniqueid')
+    predict = nn.predict(test_data)
+    predict = predict.argmax(1)
+    dfpred = pd.DataFrame({'bank_account':predict})
+    test_data['bank_account'] = dfpred
+    test_data = test_data.replace({'country': [0, 1, 2, 3]}, {'country': ['Kenya', 'Rwanda', 'Tanzania', 'Uganda'] })
+    test_data['uniqueid'] = uniqueids + ' x ' + test_data['country']
+    result = test_data[['uniqueid', 'bank_account']]
 
-print('Generating the file ...')
-for uniqueid in submissionf['uniqueid']:
-  index = result.index[result['uniqueid'] == uniqueid].tolist()[0]
-  bank = result.iloc[index]['bank_account']
-  submissionf.at[submissionf.index[submissionf['uniqueid'] == uniqueid].tolist()[0], 'bank_account'] = bank
+    print('Generating the file ...')
+    for uniqueid in submissionf['uniqueid']:
+      index = result.index[result['uniqueid'] == uniqueid].tolist()[0]
+      bank = result.iloc[index]['bank_account']
+      submissionf.at[submissionf.index[submissionf['uniqueid'] == uniqueid].tolist()[0], 'bank_account'] = bank
 
-submissionf = submissionf.replace({'bank_account': [0.0, 1.0]}, {'bank_account': ['Yes', 'No']})
+    submissionf = submissionf.replace({'bank_account': [0.0, 1.0]}, {'bank_account': ['Yes', 'No']})
 
-submissionf.to_csv('SubmissionFile.csv', index=False) 
-print('File generated !')
+    submissionf.to_csv('SubmissionFile.csv', index=False) 
+    print('File generated !')
